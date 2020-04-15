f15405d7dde527e1e8ff58c554e5e8b3793ac2e0
cschweda <cschweda@gmail.com>
2020-04-15 12:19:01 -0500

    fix: adjust new chip

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -22,15 +22,15 @@
                 :href="
                   `${$myApp.computedPublicPath}/downloads/${item.attributes.file}`
                 "
+                ><span v-if="isItNew(item)">
+                  <v-chip dark class="ma-2" x-small color="green">
+                    NEW!
+                  </v-chip></span
                 >{{ item.attributes.posted | dateFormat }} -
                 {{ item.attributes.title }}
                 <v-icon class="ml-2" small color="blue darken-2"
                   >cloud_download</v-icon
                 ></a
-              ><span v-if="isItNew(item)">
-                <v-chip class="ma-2" dark x-small color="green">
-                  NEW!
-                </v-chip></span
               >
               <ul class="summary" v-if="item.attributes.displaySummaryOnHome">
                 <li>{{ item.attributes.summary }}</li>
